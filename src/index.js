8ff8cad5c9687a88f6779754212cc231a39f85b4
ShubhamSingh3102 <shubham18singh07@gmail.com>
2025-06-12 13:45:59 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // require('dotenv').config({path: './env'})
-
+import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -7,6 +7,10 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: '/.env'
 })
+
+const app = express();
+
+app.use(express.json());
 
 connectDB()
 .then(() => {
